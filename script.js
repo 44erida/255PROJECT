45051af380e331f54b6a6e7fe24b395220f014e5
Alejandro <alejandro200761@gmail.com>
2025-12-27 21:24:09 +0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,93 @@
+// Members Data
+const membersList = [
+    {
+        name: "Alejandro Maduro",
+        section: "02",
+        id: "22401028",
+        image: "./Images/Alejandro.jpg"
+    },
+    {
+        name: "Efekan Sahin",
+        section: "04",
+        id: "22302571",
+        image: "./Images/Efekan.jpg"
+    },
+    {
+        name: "Erida Puka",
+        section: "04",
+        id: "22401239",
+        image: "./Images/Erida.jpg"
+    }
+];
+
+const membersContainer = document.getElementById('membersContainer');
+
+// Using DOM API to create the members
+membersList.forEach(member => {
+    const memberDiv = document.createElement('div');
+    memberDiv.className = "member";
+
+    const img = document.createElement('img');
+    img.src = member.image;
+    img.alt = member.name;
+
+    const detailsDiv = document.createElement('div');
+    detailsDiv.className = "details";
+
+    const nameH2 = document.createElement('h2');
+    nameH2.textContent = member.name;
+
+    const sectionP = document.createElement('p');
+    sectionP.textContent = `Section: ${member.section}`;
+
+    const idP = document.createElement('p');
+    idP.textContent = `ID: ${member.id}`;
+
+    detailsDiv.appendChild(nameH2);
+    detailsDiv.appendChild(sectionP);
+    detailsDiv.appendChild(idP);
+
+    memberDiv.appendChild(img);
+    memberDiv.appendChild(detailsDiv);
+    
+    membersContainer.appendChild(memberDiv);
+});
+
+const members = document.getElementById("members");
+const count = document.getElementById("count");
+const countDown = document.getElementById("countdown");
+const game = document.getElementById("game");
+
+let isGameStarted = false;
+
+document.body.addEventListener("click", function() {
+    if (!isGameStarted) {
+        isGameStarted = true;
+
+        members.classList.add("hidden");
+        count.classList.remove("hidden");
+
+        let timer = 3;
+        countDown.textContent = timer;
+
+        const countdownInterval = setInterval(() => {
+            timer--;
+            
+            if (timer > 0) {
+                countDown.textContent = timer;
+            } else {
+                clearInterval(timer);
+                count.classList.add("hidden");
+                game.classList.remove("hidden");
+                document.body.classList.add("body-white");
+            }
+        }, 1000);
+    }
+});
+
+function createGrid() {
+    const gameContainer = document.getElementById("gameBoard");
+    gameContainer.innerHTML = '';
+
+    
+}
